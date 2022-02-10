0f03458251f660cf07beee0d29b780c09dff9f2a
Dmitrii Torkhov <dmitriitorkhov@gmail.com>
2022-02-10 15:10:44 +0300

    rewrite lib

--- cmake/FindSQLite.cmake
+++
@@ -1,8 +0,0 @@
-set(SQLITE_ROOT ${CMAKE_CURRENT_LIST_DIR}/../)
-
-set(SQLITE_INCLUDE_DIRS ${SQLITE_ROOT})
-
-set(SQLITE_SRC
-    ${SQLITE_ROOT}/sqlite/sqlite3.c)
-
-add_library(sqlite STATIC ${SQLITE_SRC})
