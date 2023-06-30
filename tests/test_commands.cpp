052af31262222813141b06979ad7a878c5132ef3
Dmitrii Torkhov <dmitriitorkhov@gmail.com>
2023-06-30 22:16:04 +0200

    add cases to test commands

--- tests/test_commands.cpp
+++ tests/test_commands.cpp
@@ -21,8 +21,8 @@ namespace {
 
     namespace sample {
 
-        const char *string_1 = "string_1";
-        const char *string_2 = "string_2";
+        const char *chars_1 = "string_1";
+        const char *chars_2 = "string_2";
 
         const int int_1 = 1;
         const int int_2 = 2;
@@ -30,12 +30,25 @@ namespace {
         const uint32_t uint32_1 = 1;
         const uint32_t uint32_2 = 2;
 
+        const bool bool_1 = true;
+
+        const data object = {0, chars_1};
+
     }
 
     namespace query {
 
-        const char *in_vector_of_string = "data IN (string_1, string_2) ";
-        const char *in_vector_of_int = "data IN (1, 2) ";
+        const char *fields = "id data ";
+        const char *semicolon = ";";
+        const char *chars = "string_1 ";
+        const char *string = "'string_1' ";
+        const char *command = "SELECT ";
+        const char *vector_of_command = "SELECT COUNT(*) ";
+        const char *boolean = "1 ";
+        const char *integer = "1 ";
+        const char *object = ",'string_1'";
+        const char *vector_of_string = "(string_1, string_2) ";
+        const char *vector_of_int = "(1, 2) ";
 
     }
 
@@ -50,30 +63,104 @@ std::shared_ptr<sqlite::database<data>> create_db() {
 
 int main() {
 
+    // Fields
+
+    {
+        auto db = create_db();
+        *db << &data::id;
+        *db << &data::data;
+        assert(db->get_query() == query::fields);
+    }
+
+    // Char
+
+    {
+        auto db = create_db();
+        *db << ';';
+        assert(db->get_query() == query::semicolon);
+    }
+
+    // Chars
+
+    {
+        auto db = create_db();
+        *db << sample::chars_1;
+        assert(db->get_query() == query::chars);
+    }
+
+    // String
+
+    {
+        auto db = create_db();
+        *db << std::string(sample::chars_1);
+        assert(db->get_query() == query::string);
+    }
+
+    // Command
+
+    {
+        auto db = create_db();
+        *db << SELECT;
+        assert(db->get_query() == query::command);
+    }
+
+    // Vector of command
+
+    {
+        auto db = create_db();
+        *db << std::vector<command>{SELECT, COUNT};
+        assert(db->get_query() == query::vector_of_command);
+    }
+
+    // Bool
+
+    {
+        auto db = create_db();
+        *db << sample::bool_1;
+        assert(db->get_query() == query::boolean);
+    }
+
+    // Int
+
+    {
+        auto db = create_db();
+        *db << sample::int_1;
+        assert(db->get_query() == query::integer);
+    }
+
+    // Object
+
+    {
+        auto db = create_db();
+        *db << std::make_shared<data>(sample::object);
+        assert(db->get_query() == query::object);
+    }
+
     // Vector of string
 
     {
         auto db = create_db();
-        *db << &data::data << IN << std::vector<std::string>{sample::string_1, sample::string_2};
-        assert(db->get_query() == query::in_vector_of_string);
+        *db << std::vector<std::string>{sample::chars_1, sample::chars_2};
+        auto q = db->get_query();
+        assert(db->get_query() == query::vector_of_string);
     }
 
     // Vector of int
 
     {
         auto db = create_db();
-        *db << &data::data << IN << std::vector<int>{sample::int_1, sample::int_2};
+        *db << std::vector<int>{sample::int_1, sample::int_2};
         const auto q = db->get_query();
-        assert(db->get_query() == query::in_vector_of_int);
+        assert(db->get_query() == query::vector_of_int);
     }
 
     // Vector of uint32
 
     {
         auto db = create_db();
-        *db << &data::data << IN << std::vector<uint32_t>{sample::uint32_1, sample::uint32_2};
+        *db << std::vector<uint32_t>{sample::uint32_1, sample::uint32_2};
         const auto q = db->get_query();
-        assert(db->get_query() == query::in_vector_of_int);
+        assert(db->get_query() == query::vector_of_int);
     }
 
     return 0;
